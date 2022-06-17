36d0baa4f4903cdf984d85a46e6fb69541b75434
wallace sansanoski <mendessansanoski@gmail.com>
2022-06-16 23:47:59 -0300

    fixing the bug in score with js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const btnShowResult = document.querySelector('.btnShowResult')
 const divBoxTextResult = document.querySelector('.box-text-result')
 const resultOfquiz = document.querySelector('span')
 
-let score = 0
+
 
 const correctAnswer = ['B','C','A','B','B']
 
@@ -22,15 +22,13 @@ const getUserAnswers = () => {
 }
 
 const calculateScoreUser = (userAnswers) => {
+    let score = 0
     correctAnswer.forEach((answer , index) =>{
         const isUserAnswerCorrerct = answer === userAnswers[index]
         if(isUserAnswerCorrerct){
             score += 20
         }
     })
-}
-
-const animateScore = () => {
     let counter = 0 
 
    const intervalID = setInterval(() => {
@@ -41,16 +39,28 @@ const animateScore = () => {
    } , 50)
 }
 
+// const animateScore = () => {
+//     let counter = 0 
+
+//    const intervalID = setInterval(() => {
+//     if(counter === score){
+//         clearInterval(intervalID)
+//     }
+//     divBoxTextResult.textContent = `Você acertou ${counter ++} % das questões  `
+//    } , 50)
+// }
+
 const insertElementIntoDOM = () => {
     divBoxTextResult.classList.add("show")
     form.insertAdjacentElement('beforebegin' , divBoxTextResult)
 }
 
-btnShowResult.addEventListener('click' , () => {
+form.addEventListener('submit' , event => {
+    event.preventDefault()
     goTopPage()
     const userAnswers = getUserAnswers()
     calculateScoreUser(userAnswers)
-    animateScore()
+    // animateScore()
     insertElementIntoDOM()
 })
 
